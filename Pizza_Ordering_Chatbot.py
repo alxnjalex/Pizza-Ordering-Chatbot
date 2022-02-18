9e9cd4069f27690236502b820a3f542393e84790
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-02-18 13:42:35 -0800

    Added Data Validation

--- Pizza_Ordering_Chatbot.py
+++ Pizza_Ordering_Chatbot.py
@@ -1,17 +1,25 @@
 print("Hello, my name is Alex your virtual assistant. I will help you order a pizza!")
 print("I am going to ask you a few questions. After typing an answer, press enter.")
 userName = input("\nEnter your name:  ")
+while (len(userName) == 0):
+    userName = input("Please enter your name:  ,")
 if userName.lower() == "alex jones":
     print("\nMy creator, " + userName + ". Pleasure to serve you!")
 else:
     print("\nHello, " + userName + ". Nice to meet you!")
 
 size = input("\nWhat size do you want?  Enter small, medium, or large:  ")
+while size.lower() != "small" and size.lower() != "medium" and size.lower() != "large":
+    size = input("Invalid value. Please enter small, medium, or large:  ")
 flavor = input("\nEnter the flavor of pizza:  ")
 crustType = input("\nWhat type of crust do you want:  ")
 quantity = input("\nHow many of these do you want to order? Enter a numeric value:  ")
+while not quantity.isdigit():
+    quantity = input("\nValue not recognized. Please enter a numerical Value:  ")
 quantity = int(quantity)
 method = input("\nIs this carry out or delivery:  ")
+while method.lower() != "delivery" and method.lower() != "carry out" and method.lower() != "carry out":
+    method = input("\Invalid Value. Please enter delivery or carry out:  ")
 
 salesTax = 1.1
 
